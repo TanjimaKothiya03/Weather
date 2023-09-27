b7ff7dd64f30a8b5955ae3a59993c74339096f42
TanjimaKothiya <34907424+TanjimaKothiya@users.noreply.github.com>
2023-09-27 19:44:42 +0530

    Initial Commit

---
+++ Weather/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by tanjima on 27/09/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
